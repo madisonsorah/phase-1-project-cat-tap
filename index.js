d00908b615fef80f1eaa98a0dcfd3eff7bfa9447
madison sorah <madisonsorah@gmail.com>
2021-11-16 10:41:58 -0500

    Added DOMContentLoaded

--- index.js
+++ index.js
@@ -17,39 +17,45 @@
 //     })
 
 
-
-
-
-
-
-    const likeBtn = document.getElementById("like-button")
-    // // //add by one
-    const likes = document.getElementById("like-count")
-    // if(likeBtn){
-      // likeBtn.addEventListener('click', swapper, false);
-    // }
-    const timer = document.getElementById("timer")
-    const winner = document.getElementById("winner")
-    likeBtn.addEventListener('click', myfunction)
-
-    function myfunction() {
-      likes.innerHTML = parseInt(likes.innerHTML) +1 + " likes" 
-    }
+// DOMContentLoaded that loads the API data once the page loads
+document.addEventListener('DOMContentLoaded', initialize);
+function initialize() {
     
-    function move() {
+}
+// “Player Name” form that enables player to submit a username
+// “Player Name” form that submits a username of the player’s choice to the bottom of the page
+
+
+
+
+const likeBtn = document.getElementById("like-button")
+// // //add by one
+const likes = document.getElementById("like-count")
+// if(likeBtn){
+// likeBtn.addEventListener('click', swapper, false);
+// }
+const timer = document.getElementById("timer")
+const winner = document.getElementById("winner")
+likeBtn.addEventListener('click', myfunction)
+
+function myfunction() {
+    likes.innerHTML = parseInt(likes.innerHTML) +1 + " likes" 
+}
+    
+function move() {
     let seconds = 2;
-let id = setInterval(function() {
-  timer.innerHTML = parseInt(seconds--) + " seconds"
-  if(likes.innerHTML == 5 + " likes") {
-    clearInterval(id)
-    winner.textContent = "Winner"
-    console.log(winner)
-  } else if(seconds == -1) {
-    clearInterval(id)
-    winner.textContent = "Loser"
-    console.log(winner)
-  }
-}, 1000)};
+    let id = setInterval(function() {
+    timer.innerHTML = parseInt(seconds--) + " seconds"
+    if(likes.innerHTML == 5 + " likes") {
+        clearInterval(id)
+        winner.textContent = "Winner"
+        console.log(winner)
+    } else if(seconds == -1) {
+        clearInterval(id)
+        winner.textContent = "Loser"
+        console.log(winner)
+    }
+    }, 1000)};
     
     // Event listeners: 
     // DOMContentLoaded that loads the API data once the page loads
