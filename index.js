151bd708d506195d83b79558ed63e0eea9585de9
madison sorah <madisonsorah@gmail.com>
2021-11-15 20:04:57 -0500

    Started adding HTML & styling

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+let catImagesUrls = [];
+
 fetch("https://cataas.com/api/cats?tags=cute")
     .then ((response) => response.json())
-    .then ((catImages) => console.log(catImages))
-
+    .then ((catImages) => catImages.map((catImage) => catImage.id))
+    .then((catIds) => {
+        catIds.forEach((catId) => {
+            catImagesUrls.push(`https://cataas.com/api/cat/${catId}`)
+        })
+        console.log(catImagesUrls);
+    })
