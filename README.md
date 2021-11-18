ab125d7bbb26873192c6b570b349f3778b14cc0e
madison sorah <madisonsorah@gmail.com>
2021-11-18 16:10:23 -0500

    Cleaned up code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # phase-1-project-cat-tap
-Cat Tap project
+
+HOW TO PLAY CAT TAP:
+
+1. Choose a username and click confirm
+2. Click start to begin playing
+3. Try to click the random cat image that appears on the game board as many times as you can to increase score
+4. If you click outside of the random cat image, the game will stop 
+5. If you choose to retry, your score will be set back to zero
+6. Good luck! 
--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 // DOMContentLoaded that loads the API data once the page loads
+
 document.addEventListener('DOMContentLoaded', initialize);
 function initialize() {
     fetchData();
 }
 
 // Fetch API data
-const catImagesUrls = [];
-// <<<<<<< HEAD
-const menu = document.getElementById('cat-menu')
 
-// =======
-// const menu = document.getElementById("cat-menu")
-// >>>>>>> 4319a1a (score)
+const catImagesUrls = [];
+const menu = document.getElementById('cat-menu')
 
 function fetchData() {
     fetch('https://cataas.com/api/cats?tags=cute')
@@ -21,18 +18,11 @@ function fetchData() {
         catIds.forEach((catId) => {
             catImagesUrls.push(`https://cataas.com/cat/${catId}`)
         })
-        // catImagesUrls.splice(1,80)
-        // console.log(catImagesUrls);
-        // const newImage = document.createElement('img')
-        // newImage.src = catImagesUrls[1]
-        
-        // const img = document.querySelector("img"); 
-        // img.src = catImagesUrls[0]
     })
 }
 
-
 // “Player Name” form that enables player to submit a username
+
 const playerNameForm = document.querySelector('#username-input')
 const playerName = document.querySelector('#username')
 
@@ -43,6 +33,7 @@ playerNameForm.addEventListener('submit', (e) => {
 })
 
 // “Player Name” form that submits a username of the player’s choice to the bottom of the page
+
 function playerNameInput(userName) {
     playerName.innerHTML = '';
     playerName.textContent = `${userName}`;
@@ -54,13 +45,11 @@ function playerNameInput(userName) {
 
 // “Start game” button click event 
 // "Start game" button disappears once clicked and game starts
-// {/* <button id="start-game" type="text">START GAME</button> */}
+
 const startGame = document.getElementById('start-game')
 const score = document.querySelector('#score-count')
 const finalScore = document.querySelector('#final-score')
-const named = document.querySelector('#name')
 const resetButton = document.querySelector('#retry')
-// const randomTimer = (min, max) => { return Math.floor(Math.random() * (max - min + 1)) + min}
 const randomTimeout = Math.floor((Math.random() + 1) * 2000)
 startGame.addEventListener('click', gameStarted)
 
@@ -68,44 +57,44 @@ startGame.addEventListener('click', gameStarted)
 // Update number of clicks in the "#score-count" element
 // Set timeout on cat image; when cat image disappears, another cat image reappears somewhere different on the game board
 // Resize cat images to fit on the game board
+
 function gameStarted(e) {
     e.stopPropagation();
+    playerNameForm.style.display = 'none';
     startGame.style.display = 'none';
     resetButton.style.display = 'none';
+    score.textContent = 'Score: 0';
     finalScore.style.display = 'none';
-    named.style.display = ""
     const firstRandomImageNumber = Math.floor(Math.random() * catImagesUrls.length);
     const firstCatImg = document.createElement('img');
     const firstCatContainer = document.querySelector('#cat-menu');
-    firstCatImg.src = catImagesUrls[firstRandomImageNumber]
+    firstCatImg.src = catImagesUrls[firstRandomImageNumber];
     firstCatImg.style.position = 'absolute';
     firstCatImg.style.top = firstCatContainer.clientHeight * Math.random() + 'px';
     firstCatImg.style.right = firstCatContainer.clientWidth * Math.random() + 'px';
     firstCatImg.style.bottom = firstCatContainer.clientHeight * Math.random() + 'px';
     firstCatImg.style.left = firstCatContainer.clientWidth * Math.random() + 'px';
     firstCatContainer.appendChild(firstCatImg);
-    // setTimeout(function(){firstCatImg.style.display = "none"}, randomTimer(5000, 7000))
     setTimeout(function() {firstCatImg.style.display = 'none'}, 1000);
 
     const interval = setInterval(function() {
         const randomImageNumber = Math.floor(Math.random() * catImagesUrls.length);
         const catImageContainer = document.querySelector('#cat-menu'); 
-        const img = document.createElement('img')
-        img.src = catImagesUrls[randomImageNumber]
+        const img = document.createElement('img');
+        img.src = catImagesUrls[randomImageNumber];
         img.style.position = 'absolute';
         img.style.top = catImageContainer.clientHeight * Math.random() + 'px';
         img.style.right = catImageContainer.clientWidth * Math.random() + 'px';
         img.style.bottom = catImageContainer.clientHeight * Math.random() + 'px';
         img.style.left = catImageContainer.clientWidth * Math.random() + 'px';
         catImageContainer.appendChild(img);
-        // setTimeout(function(){img.style.display = "none"}, randomTimer(5000, 7000))
     
         img.addEventListener('click', clicked)
         function clicked(e) {
             e.stopPropagation();
-            const scoreCount = parseInt(score.textContent.split(' ')[1])
-            score.textContent = `Score: ${scoreCount + 1 }`
-            finalScore.textContent = `Final Score: ${scoreCount + 1 }`
+            const scoreCount = parseInt(score.textContent.split(' ')[1]);
+            score.textContent = `Score: ${scoreCount + 1 }`;
+            finalScore.textContent = `Final Score: ${scoreCount + 1 }`;
         };
         
         setTimeout(function() {img.style.display = 'none'}, randomTimeout);
@@ -115,32 +104,24 @@ function gameStarted(e) {
     backgrounds.addEventListener('click', outsideClick)
 
     function outsideClick() {
-        clearInterval(interval);
         // Clear images
-
-        // Display userName
-        named.textContent = ""
+        clearInterval(interval);
         // Display player score at center of screen
-        finalScore.style.display= 'block'
+        finalScore.style.display= 'block';
         // Display retry button
-        resetButton.style.display= 'block'
+        resetButton.style.display= 'block';
         // Restart game
-        resetButton.addEventListener('click', gameRestarted)
-    }
+        resetButton.addEventListener('click', gameRestarted);
+    };
 }
-
 
 // Create a game board click event that stops the game if the space outside of cat images is clicked
 // Once game stops, player score is displayed in middle of screen + retry button is shown underneath
 // Once retry button is clicked, the game resets with the user's original Player Name
 
 function gameRestarted(e) {
-    e.stopPropagation()
+    e.stopPropagation();
     resetButton.style.display = 'none';
     finalScore.style.display = 'none';
-    named.style.display = " none"
-    gameStarted(e)
-    window.location.reload();
-    return false
-    // score.textContent = `Score: ${scoreCount = 0}`
+    gameStarted(e);
 }
