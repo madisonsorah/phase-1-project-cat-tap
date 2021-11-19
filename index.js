cc897ed7c5f4a9691e0c459d2a273c9a02aeea7d
Hoa Le <Hoa_le9485@yahoo.com>
2021-11-19 16:38:44 -0500

    randomCATS

--- index.js
+++ index.js
@@ -126,3 +126,16 @@ function gameRestarted(e) {
     finalScore.style.display = 'none';
     gameStarted(e);
 }
+
+//button for random image
+let randomImages = document.getElementById("images")
+randomImages.addEventListener('click', randomCats)
+
+function randomCats(e) {
+    const randomImageNumber = Math.floor(Math.random() * catImagesUrls.length);
+    const img = document.createElement('img');
+    img.src = catImagesUrls[randomImageNumber]
+    randomImages.append(img)
+    console.log(randomCats)
+    setTimeout(function() {img.style.display = 'none'}, randomTimeout);
+}
